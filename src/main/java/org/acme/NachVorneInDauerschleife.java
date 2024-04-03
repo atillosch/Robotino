23967f2bf16676c26f6bd238502724e9a3903389
atillosch <catilla0027@gmail.com>
2024-04-03 16:49:23 +0200

    Bessere Sprache

--- src/main/java/org/acme/NachVorneInDauerschleife.java
+++ src/main/java/org/acme/NachVorneInDauerschleife.java
@@ -9,7 +9,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-public class NachVorneAMK {
+public class NachVorneInDauerschleife {
 
     public static void main(String[] args) {
         try {
